226a11c060e8223aa02e6a1903806bb58c388018
Nagendra Chaudhary <nagendra.chaudhary@example.com>
2025-03-10 17:25:41 -0500

    vue-slots-intro

---
+++ learn-vue-3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AlertBox></AlertBox>
+  <AlertBox>Invalid Username or password. Please try again!</AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again.</h1>
+  </AlertBox>
+</template>
+
+<script setup>
+import AlertBox from './AlertBox.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
