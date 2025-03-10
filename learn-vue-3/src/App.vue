d7ff163fe96abd5e8ee6cd73ee0ccead10f2e23c
Nagendra Chaudhary <nagendra.chaudhary@example.com>
2025-03-10 17:27:16 -0500

    vue-named-slots

---
+++ learn-vue-3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <BaseLayout>
+  <template #header>
+    <h1>Here might be a page title.</h1>
+  </template>
+  <!-- Anything without a template will be default or we can give it the #default title -->
+  <template #default>
+    <p>A paragraph of the main content.</p>
+    <p>And another one.</p>
+  </template>
+  <template #footer>
+    <p>Here's some contact info.</p>
+  </template>
+  </BaseLayout>
+</template>
+
+<script setup>
+import BaseLayout from './BaseLayout.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
