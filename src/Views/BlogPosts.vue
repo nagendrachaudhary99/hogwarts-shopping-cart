13eb8cbbf73742d4dfb681ee978eb76895c6243e
Nagendra Chaudhary <nagendrachaudhary0308@gmail.com>
2025-04-22 16:11:43 -0500

    trasnsitionvue-rout er-transition

---
+++ src/Views/BlogPosts.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="blog-posts-layout">
+        <aside class="blog-posts-list">
+            <BlogPostsList></BlogPostsList>
+        </aside>
+        <main class="blog-posts-content-with-sidebar">
+            <router-view class="blog-posts-content" v-slot="{ Component,route }">
+                <transition
+                enter-active-class="animate__animated animate__fadeIn"
+                leave-active-class="animate__animated animate__fadeOut"
+                mode="out-in">
+                    <component :is="Component" :key="route.path"></component>
+                </transition>
+            </router-view>
+            <router-view name="sidebar"></router-view>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import BlogPostsList from '@/components/BlogPostList.vue';
+</script>
+
+<style lang="scss" scoped>
+    .blog-posts-layout{
+        display: flex;
+        gap:1rem;
+        width: 100%;
+    }
+
+    .blog-posts-list{
+        flex: 0 0 25%;
+    }
+
+    .blog-posts-content-with-sidebar{
+        display: flex;
+        gap:1rem;
+        flex: 0 0 75%;
+
+        .blog-posts-content{
+            flex: 1;
+        }
+    }
+
+</style>
