454b3bdb2ea0796ea6ddc0e8961b9cb27daeeaab
Nagendra Chaudhary <nagendrachaudhary0308@gmail.com>
2025-04-21 22:41:47 -0500

    vue_with_Bingyang-vue-router-navigation-guards

---
+++ src/Views/BlogPosts.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="blog-posts-layout">
+        <aside class="blog-posts-list">
+            <BlogPostsList></BlogPostsList>
+        </aside>
+        <main class="blog-posts-content-with-sidebar">
+            <router-view class="blog-posts-content"></router-view>
+            <router-view name="sidebar"></router-view>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import BlogPostsList from '@/components/BlogPostList.vue';
+</script>
+
+<style lang="scss" scoped>
+    .blog-posts-layout{
+        display: flex;
+        gap:1rem;
+        width: 100%;
+    }
+
+    .blog-posts-list{
+        flex: 0 0 25%;
+    }
+
+    .blog-posts-content-with-sidebar{
+        display: flex;
+        gap:1rem;
+        flex: 0 0 75%;
+
+        .blog-posts-content{
+            flex: 1;
+        }
+    }
+</style>
